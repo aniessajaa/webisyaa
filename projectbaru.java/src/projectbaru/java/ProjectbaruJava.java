d300343a13bca64f86a8745e4e21f0b77b3b74af
aniessajaa <annisaaaaawjyy26@gmail.com>
2025-07-24 07:01:39 +0700

    Add files via upload

---
+++ projectbaru.java/src/projectbaru/java/ProjectbaruJava.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectbaru.java;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ardiw
+ */
+public class ProjectbaruJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//System.out.println("Halo"+" "+"Dunia"); 
+//System.out.println(2+" "+5); 
+//System.out.println(2+5); 
+//System.out.println("Angka "+" "+5); 
+//System.out.println("2 + 5"+" = "+2+5); 
+//System.out.println("2 + 5"+" = "+(2+5));
+//    }
+   
+//        String namaDepan = "";
+//        String namaBelakang = "+";
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Nama Depan : ");
+//        namaDepan = input.next();
+//        System.out.print("Nama Belakang : ");
+//        namaBelakang = input.next();
+//        System.out.println("Nama Saya adalah :");
+//        System.out.println(namaDepan + " " + namaBelakang);
+//    }
+}
+
+
+//public class ProjectBaruJava {
+//    public static void main(String[] args) {
+//        JOptionPane.showMessageDialog(
+//            null,
+//            "Sebuah Pertanyaan.",
+//            "Pertanyaan",
+//            JOptionPane.QUESTION_MESSAGE
+//        );
+//    }
+//}
+
+
+}
+
+    
+    
+
